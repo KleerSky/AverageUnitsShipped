f04a546578d50447db14cae280f4388d4ea6bdef
Benjamin <Benjamin@T470-BENJAMINPR>
2018-03-07 02:15:13 -0500

    Add project files.

---
+++ AverageUnitsShipped/AverageUnitsShipped.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.tbInput = New System.Windows.Forms.TextBox()
+        Me.tbSoldHolder = New System.Windows.Forms.TextBox()
+        Me.lblDayNumber = New System.Windows.Forms.Label()
+        Me.frmAverageUnitsShippedToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(17, 364)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 6
+        Me.btnEnter.Text = "&Enter"
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.btnEnter, "Click to enter daily sales")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(123, 364)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 7
+        Me.btnReset.Text = "&Reset"
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.btnReset, "Click to reset form and input data")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(229, 364)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "E&xit"
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.btnExit, "Click to close form and end program")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblUnits
+        '
+        Me.lblUnits.Location = New System.Drawing.Point(33, 21)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(44, 17)
+        Me.lblUnits.TabIndex = 0
+        Me.lblUnits.Text = "&Units:"
+        Me.lblUnits.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.lblUnits, "Input units sold here")
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverage.Location = New System.Drawing.Point(29, 239)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(255, 66)
+        Me.lblAverage.TabIndex = 5
+        Me.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.lblAverage, "Displays the calculated average, once all entries are made")
+        '
+        'tbInput
+        '
+        Me.tbInput.Location = New System.Drawing.Point(96, 19)
+        Me.tbInput.Name = "tbInput"
+        Me.tbInput.Size = New System.Drawing.Size(100, 22)
+        Me.tbInput.TabIndex = 1
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.tbInput, "Input your sales for current day here.")
+        '
+        'tbSoldHolder
+        '
+        Me.tbSoldHolder.Location = New System.Drawing.Point(25, 60)
+        Me.tbSoldHolder.Multiline = True
+        Me.tbSoldHolder.Name = "tbSoldHolder"
+        Me.tbSoldHolder.Size = New System.Drawing.Size(263, 144)
+        Me.tbSoldHolder.TabIndex = 4
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.tbSoldHolder, "Contains all entered sales for previous days")
+        '
+        'lblDayNumber
+        '
+        Me.lblDayNumber.Location = New System.Drawing.Point(213, 22)
+        Me.lblDayNumber.Name = "lblDayNumber"
+        Me.lblDayNumber.Size = New System.Drawing.Size(91, 17)
+        Me.lblDayNumber.TabIndex = 3
+        Me.lblDayNumber.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me.lblDayNumber, "This label shows your current day of the week")
+        '
+        'frmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(325, 426)
+        Me.Controls.Add(Me.lblDayNumber)
+        Me.Controls.Add(Me.tbSoldHolder)
+        Me.Controls.Add(Me.tbInput)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.frmAverageUnitsShippedToolTip.SetToolTip(Me, "Someone's bored ... This is a form!")
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents tbInput As TextBox
+    Friend WithEvents tbSoldHolder As TextBox
+    Friend WithEvents lblDayNumber As Label
+    Friend WithEvents frmAverageUnitsShippedToolTip As ToolTip
+End Class
---
+++ AverageUnitsShipped/AverageUnitsShipped.vb
@@ -0,0 +1,106 @@
+﻿''' <summary>
+''' Program: Average Units Shipped
+''' Author: Benjamin Priebe - 100437762
+''' Description: This program takes an input of one week 
+''' of units shipped out, per day, stores them in an array,
+''' and then once we entered the seventh day's information,
+''' to program will calculate the average units shipped for
+''' the week and display it rounded to two decimals.
+'''
+''' </summary>
+
+Option Strict On    'Turn strict on
+Public Class frmAverageUnitsShipped
+
+    Shared employeeCounter As Integer = 0
+    'CONSTANTS
+    Dim InputLowerLimit As Integer = 0  'Constant inputLowerLimit as 0
+    Dim InputUpperLimit As Integer = 1001   'Constant inputUpperLimit as 1001
+
+    'VARIABLES
+    Dim userInput As String = String.Empty  'Create empty string for user input
+    Dim userInputInteger As Integer = 0     'Create variable for the TryParse to output sanitized number to
+    Dim array(6) As Integer                 'Create array with 7 spaces
+    Dim index As Integer = 0                'Create integer to use as index counter 
+    Dim sum As Integer = 0                   'Create integer to use as sum in average calculations
+    Dim average As Double = 0         'Create integer to use as result of the average
+
+    Private Sub frmAverageUnitsShipped_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblDayNumber.Text = "Day 1" 'On form load, print "Day 1"
+    End Sub
+    Private Sub Enter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        userInput = tbInput.Text    'Save the text in the input textbox to the userInput string variable
+
+        If Not (Integer.TryParse(userInput, userInputInteger)) Then 'Check if userInput parses, userInputInteger is a throwaway number
+            'because the parse needs an overload
+
+            MsgBox("ERROR: Input must be a whole number!")  'Show error to user in MessageBox
+            index -= 1  'Set the index counter back one, go again
+            tbInput.SelectAll() 'If invalid, select all text to write over
+            tbInput.Focus()     'Reset focus to input box after each entry
+        Else
+            'Convert the userInput to an int, if it is less than the input's lowerLimit or higher than the upperLimit,
+            'run the error message below - ELSE, continue with processing
+            If Not (userInputInteger < InputLowerLimit Or userInputInteger >= InputUpperLimit) Then
+
+                lblDayNumber.Text = "Day " & (index + 2)    'Updates the running day counter, adds an extra one so that
+                'form displays day user in entering for, not that day previously entered
+
+                array(index) = (userInputInteger)    'Store userInput in the current space in the array
+
+                tbSoldHolder.AppendText(array(index) & vbCrLf)  'Print the userInput to the next line in tbSoldHolder
+                tbInput.Text = ""   'If valid, blank the input textbox for new entry
+                tbInput.Focus()     'Reset focus to input box after each entry
+
+                If index = array.Length - 1 Then 'If we have hit the end of our week, do the following (processing)
+
+                    array(index) = userInputInteger   'Save userInput into the current index in the array
+                    tbInput.Enabled = False     'Disable the input textbox, at end of entry cycle
+                    btnEnter.Enabled = False    'Disable the Enter button, at end of entry cycle
+
+                    For Each element In array   'Loop to calculate sum of array
+                        sum += element  'For every 'element' in the array, add it into the sum (total)
+                    Next
+
+                    averageResult = (sum / array.Length)    'Determine the average from sum
+                    averageResult = Math.Round((averageResult), 2)    'Round the average to two decimals
+                    lblAverage.Text = "The average sales per day is " & averageResult  'Print to user in output label
+                    lblDayNumber.Text = "Complete!" 'Change the lblDayNumber text property to "Complete!" signifying end of entry cycle
+                    tbInput.Text = ""   'Blank the input textbox
+                End If
+            Else
+                MsgBox("ERROR: Input must be between 0 and 1000!")  'Show error to user in MessageBox
+                index -= 1  'Set the index counter back one, go again
+                tbInput.SelectAll() 'Highlight all text in box, so you can write over it easily
+                tbInput.Focus()     'Reset focus to input box after each entry
+            End If
+        End If
+        index += 1      'Advance the index counter by one
+    End Sub
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ResetMethod()   'Call what is contained in the resetmethod()
+    End Sub
+    Private Sub ResetMethod()
+
+        index = 0   'Set index back to 0
+        sum = 0     'Empty the sum variable
+        averageResult = 0 'Reset the average variable to 0
+        tbInput.Focus() 'Set focus to input textbox
+        lblAverage.Text = String.Empty    'Empty the lblAverage
+        tbSoldHolder.Text = ""  'Empty the textbox which holds inputs
+        lblDayNumber.Text = "Day " & (index + 1) 'Set day label back to one
+        tbInput.Enabled = True  'Disable the input textbox
+        btnEnter.Enabled = True 'Disable the enter button
+        userInput = String.Empty    'Reset userInput to empty string
+        userInputInteger = 0        'Reset the TryParse output integer for userInput to 0
+        tbInput.Text = ""   'Empty text in input textbox
+
+        For index As Integer = 0 To array.Length - 1    'Instructions inside
+            'loop through array by the index, using the 7th element (6)
+            array(index) = 0 'and set each to zero
+        Next index  'Go to next index, loop again
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()  'Close currently open form
+    End Sub
+End Class
